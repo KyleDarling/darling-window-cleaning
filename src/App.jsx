9f743a65c56a6b790ebc7a9531ae412dc3a8dd0d
KyleDarling <kyle.b.darling24@gmail.com>
2026-03-23 13:32:19 -0700

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div style={{fontFamily: "Arial", padding: "40px"}}>
+      <h1>Darling Window Cleaning</h1>
+      <p>Clean windows. Fast service. Easy booking.</p>
+      <a href="tel:9497046019">Call (949) 704-6019</a>
+    </div>
+  );
+}
